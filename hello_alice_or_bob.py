7e7c6840584454c83ffa9fb71bfda67b0b58b79e
Lwazi Radebe <lwaziradebe100@gmail.com>
2021-09-16 10:37:02 +0200

    greet only Alice or Bob

---
+++ hello_alice_or_bob.py
@@ -0,0 +1,10 @@
+def hello():
+
+    name = input('Enter your name: ')
+
+    if name == 'Alice':
+        print(f'Hello {name}!')
+    elif name == 'Bob':
+        print(f'Hello {name}!')
+
+hello()
